4998658c05fc756eb8d58a88d66ac1e9fa9654d5
BuildTools <unconfigured@null.spigotmc.org>
2020-06-14 18:00:20 +0200

    Added promiseSleep

--- docs/examples.md
+++ docs/examples.md
@@ -36,3 +36,18 @@ console.log(blueutilities.replaceAll("Hello, World!", "World", "world"));
 ```
 Hello, world!
 ```
+## promiseSleep
+### Input
+```js
+const blueutilities = require('blueutilities');
+console.log("Will wait 1 sec");
+blueutilities.promiseSleep(1000).then(() => {
+    console.log("1 sec passed!");
+});
+```
+### Output
+```
+Will wait 1 sec
+*One second passes*
+1 sec passed!
+```
--- index.js
+++ index.js
@@ -21,7 +21,17 @@ function setupReplaceAll() {
     String.prototype.replaceAll = function(textReplace, textReplace2) {
         return this.split(textReplace).join(textReplace2).toString();
     }
-}
+};
+function promiseSleep(ms) {
+    if(isNaN(ms)) throw new Error("Incorrect usage! Correct usage: blueutilities.promiseSleep(Number)");
+    if(isNaN(ms)) return;
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve();
+        }, ms)
+    });
+};
 module.exports.safeEval = safeEval;
 module.exports.replaceAll = replaceAll;
 module.exports.setupReplaceAll = setupReplaceAll;
+module.exports.promiseSleep = promiseSleep;
