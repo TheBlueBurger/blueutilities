031d2d481cce4b0229314bed32d73d700056916d
BlueBurgersTDD <58081413+BlueBurgersTDD@users.noreply.github.com>
2021-05-29 12:46:19 +0200

    remove "types" folder from github

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface safeEvalReturned {
-    error: boolean;
-    output: string;
-}
-declare function safeEval(evalcode: string): Promise<safeEvalReturned>;
-declare function replaceAll(text: string, textReplace: string, textReplace2: string): string;
-declare function setupReplaceAll(): void;
-declare function promiseSleep(ms: number): Promise<void>;
-export { promiseSleep, setupReplaceAll, replaceAll, safeEval };
